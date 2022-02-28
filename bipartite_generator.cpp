2b19f92b6a3cba3fd1318ea2a952bd46d43e3cfd
Daniel Vonrhein <danjvon@gmail.com>
2022-02-28 15:39:27 -0800

    replaced the generator's build file with its source file in git

---
+++ bipartite_generator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+
+int main(){
+    int l, r, m;
+    l = 10; r = 10; m = 16;
+    srand(5);
+    std::ofstream str;
+    str.open("test.txt");
+    int firstNode, secondNode, fee;
+    str << l << " " << r << " " << m << std::endl;
+    for (int i = 0; i < m; ++i){
+        firstNode = (rand() % l ) + 1;
+        secondNode = (rand() % r ) + 1;
+        
+        str << firstNode  << " " << secondNode << std::endl;
+    }
+    
+    str.close();
+
+    return 0;
+   
+}
